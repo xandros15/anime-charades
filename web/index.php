202920532a6960803c3f74c6e3572adf2e45a47c
xandros15 <xandros92@gmail.com>
2018-04-10 21:14:24 +0200

    add twig to application

--- web/index.php
+++ web/index.php
@@ -10,6 +10,22 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 
 $slim = new App();
+
+$container = $slim->getContainer();
+
+// Register Twig View helper
+$container['view'] = function (\Slim\Container $container) {
+    $view = new \Slim\Views\Twig(__DIR__ . '/../templates', [
+//        'cache' => 'path/to/cache',
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new \Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
 $slim->get('[/]', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
 
     $app = new AnimeCharades();
