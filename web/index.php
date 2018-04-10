225332f21fccb9c1d7ad23a740316d211c86d495
xandros15 <xandros92@gmail.com>
2018-04-10 21:27:22 +0200

    add manager aliases

--- web/index.php
+++ web/index.php
@@ -2,13 +2,13 @@
 
 use App\AnimeCharades;
 use App\AnimeListManager;
+use App\Database\SQLiteConnection;
 use Slim\App;
 use Slim\Exception\NotFoundException;
 
 session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
 
 $slim = new App();
 
@@ -44,6 +44,59 @@ $slim->get('[/]', function (\Slim\Http\Request $request, \Slim\Http\Response $re
     ]);
 });
 
+$slim->get('/anime', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
+
+    $connection = (new SQLiteConnection())->connect();
+    $page = $request->getParam('p', 1);
+    $search = $request->getParam('s', '');
+    if ($search) {
+        $offset = (int) ($page - 1) * 50;
+        $stmt = $connection->prepare("SELECT * FROM anime WHERE `name` LIKE ? ORDER BY `name` LIMIT 50 OFFSET " . $offset);
+        $stmt->execute(['%' . $search . '%']);
+        $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } else {
+        $offset = (int) ($page - 1) * 500;
+        $items = $connection->query("SELECT * FROM anime ORDER BY name LIMIT 500 OFFSET " . $offset, PDO::FETCH_ASSOC);
+    }
+
+    return $this->view->render($response, 'list.twig', ['items' => $items, 'page' => $page]);
+})->setName('anime.index');
+
+$slim->post('/anime/update/{id:\d+}', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
+    $id = (int) $request->getAttribute('id');
+    $name = (string) trim($request->getParam('name', ''));
+    if (!$name) {
+        throw new NotFoundException($request, $response);
+    }
+    $connection = (new SQLiteConnection())->connect();
+    $stmt = $connection->prepare("UPDATE anime SET main = ? WHERE id = ?");
+    $stmt->execute([$name, $id]);
+
+    return $this->view->render($response, '_delete-form.twig', ['id' => $id, 'main' => $name]);
+})->setName('anime.update');
+
+$slim->post('/anime/delete/{id:\d+}', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
+    $id = (int) $request->getAttribute('id');
+    $connection = (new SQLiteConnection())->connect();
+    $stmt = $connection->prepare("UPDATE anime SET main = NULL WHERE id = ?");
+    $stmt->execute([$id]);
+
+    return $this->view->render($response, '_update-form.twig', ['id' => $id]);
+})->setName('anime.delete');
+
+$slim->get('/anime/hint', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
+
+    if (!$name = $request->getParam('query')) {
+        throw new NotFoundException($request, $response);
+    }
+    $connection = (new SQLiteConnection())->connect();
+    $stmt = $connection->prepare("SELECT name FROM anime WHERE name LIKE ? ORDER BY name LIMIT 10");
+    $stmt->execute(['%' . $name . '%']);
+    $payload = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+    return $response->withJson($payload);
+})->setName('anime.hint');
+
 $slim->get('/fetch', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
     $nicknames = [
     ];
@@ -58,4 +111,3 @@ $slim->get('/fetch', function (\Slim\Http\Request $request, \Slim\Http\Response 
 
 
 $slim->run();
-
